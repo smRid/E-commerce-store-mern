ea9bec49744adc74d7b2bd1894ed7e70480b71e9
smRid <omorsk911@gmail.com>
2025-05-12 15:20:40 +0600

    auth, redis, access & refresh token added

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // Parse JSON bodies 
+app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
+
+    connectDB();
 });
 
